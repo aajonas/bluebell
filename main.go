fa9b5b5f9e5a5f1e89b9b425d702309a6a497186
aajons <87728875@qq.com>
2020-11-12 11:10:03 +0800

    初始化mysql

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bluebell/dao/mysql"
 	"bluebell/logger"
 	"bluebell/settings"
 	"fmt"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -12,9 +14,17 @@ func main() {
 		fmt.Printf("init settings failed, err:%v\n", err)
 		return
 	}
-	//2、设置日志库
+	//2、初始化日志
 	if err := logger.Init(settings.Conf.LogConfig); err != nil {
 		fmt.Printf("init logger failed, err:%v\n", err)
 		return
 	}
+	defer zap.L().Sync()
+	zap.L().Debug("logger init success...")
+	//3、初始化MySQL连接
+	if err := mysql.Init(settings.Conf.MySQLConfig); err != nil {
+		fmt.Printf("init mysql failed, err:%v\n", err)
+		return
+	}
+	defer mysql.Close()
 }
